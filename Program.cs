8b0447f4ba0c93c116cb7f242134076e1146d683
Rameel Haroon <RameelHaroonOff@gmail.com>
2024-12-30 22:38:49 +0500

    Added GameDto and some test data

--- Program.cs
+++ Program.cs
@@ -1,5 +1,24 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+List<GameDto> games = [
+    new (
+        1,
+        "Street Fighter 11",
+        "Fighting",
+        19.99M,
+        new DateOnly(1997, 7, 15)
+    ),
+    new (
+        2,
+        "Final Fantasy XIV",
+        "Roleplaying",
+        59.99M,
+        new DateOnly(2010, 9, 30)
+    )
+];
 
 app.MapGet("/", () => "Hello Rameel!");
 
--- obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+366767847258002a59f536a5a5fb6d4e3ea83b5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
